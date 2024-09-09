e2ab657f127956465af28474a3748f3608312a3a
Rodrigo Suguimoto <rodrigo.suguimoto@10903-MB.local>
2024-09-09 07:53:22 -0300

    Finished stage 1

--- Simple Search Engine (Java)/task/src/search/Main.java
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -1,7 +1,22 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        String firstLine = scanner.nextLine();
+        String wordToSearchFor = scanner.next();
+
+        String[] words = firstLine.split(" ");
+        String response = "Not found";
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(wordToSearchFor)) {
+                response = String.format("%d", i + 1);
+            }
+        }
+
+        System.out.println(response);
     }
 }
